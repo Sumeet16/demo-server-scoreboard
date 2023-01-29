1ec50ac5821e430eadf7d0b260e621fcd80f30a2
Sumeet Yadav <sumeetishwar1610@gmail.com>
2023-01-29 18:00:33 +0530

    second commit

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const connectToDB = require("./db/conn");
+const userModel = require("../model/user.model");
 
 // middlewares
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/',require('./routes/getScore'))
-app.use('/',require('./routes/updateScore'))
 
 // defining port
 
@@ -20,6 +19,31 @@ app.get("/", (req, res) => {
     message: "You've come to the right place... it's a GET request sent to the API!!",
   });
 });
+
+app.get('/getScore', async (req, res) => {
+  try {
+    const scores = await userModel.find();
+    return res.json({ message: "All scores are sent! 🟢", scores: scores })
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ message: "Some error occurred while fetching scores! 🔴" });
+  }
+})
+
+app.post('/updateScore', async (req, res) => {
+  const { currentScoreLose, currentScoreWin, name } = req.body;
+  const totalScore = ((currentScoreLose * 10) + (currentScoreWin * 20))
+
+  try {
+    const Scores = await userModel.findOneAndUpdate({ team: name }, { $set: { "win": currentScoreWin, "lose": currentScoreLose, "point": totalScore } }, { new: true });
+    return res.json({ message: "Scores is updated! 🟢", score: Scores })
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ message: "Some error occurred while updating the score! 🔴" });
+  }
+})
 
 // Starting Server on PORT
 app.listen(8080, () => console.log("Server started on PORT Number: " + 8080 + " 🟢"));
--- routes/getScore.js
+++ routes/getScore.js
@@ -5,11 +5,11 @@ const userModel = require("../model/user.model");
 router.get('/getScore', async (req, res) => {
     try {
         const scores = await userModel.find();
-        return res.json({message: "All scores are sent! 🟢", scores: scores})
+        return res.json({ message: "All scores are sent! 🟢", scores: scores })
     } catch (error) {
         return res
-        .status(401)
-        .json({ message: "Some error occurred while fetching scores! 🔴" });
+            .status(401)
+            .json({ message: "Some error occurred while fetching scores! 🔴" });
     }
 })
 
