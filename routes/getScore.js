d7753a39636939875a2c95999866c7e5e3051d3a
Sumeet Yadav <sumeetishwar1610@gmail.com>
2023-01-29 21:37:40 +0530

    second commit -2

--- routes/getScore.js
+++ routes/getScore.js
@@ -2,15 +2,19 @@ const express = require('express');
 const router = express.Router();
 const userModel = require("../model/user.model");
 
-router.get('/', async (req, res) => {
-    try {
-        const scores = await userModel.find();
-        return res.json({ message: "All scores are sent! 🟢", scores: scores })
-    } catch (error) {
-        return res
-            .status(401)
-            .json({ message: "Some error occurred while fetching scores! 🔴" });
-    }
+router.get('/', (req, res) => {
+    // try {
+    //     const scores = await userModel.find();
+    //     return res.json({ message: "All scores are sent! 🟢", scores: scores })
+    // } catch (error) {
+    //     return res
+    //         .status(401)
+    //         .json({ message: "Some error occurred while fetching scores! 🔴" });
+    // }
+    return res.status(200).json({
+        title: "Express Testing",
+        message: "The app is working properly!",
+    });
 })
 
 module.exports = router
